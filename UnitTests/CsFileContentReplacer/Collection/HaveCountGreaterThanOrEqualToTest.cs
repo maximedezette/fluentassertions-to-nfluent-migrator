350edaecdf8ee496668e4eb3449f7001d77b4a66
maxime.dezette <m.dezette@gmail.com>
2025-01-24 21:28:49 +0100

    chore: group collection tests :recycle:

---
+++ UnitTests/CsFileContentReplacer/Collection/HaveCountGreaterThanOrEqualToTest.cs
@@ -0,0 +1,17 @@
+﻿using NFluent;
+
+namespace UnitTests.CsFileContentReplacer;
+
+public class HaveCountGreaterThanOrEqualToTest : CsTestContentReplacerTest
+{
+    [Fact]
+    public void Should_replace_HaveCountGreaterThanOrEqualTo()
+    {
+        const string fluentAssertions = "var.Should().HaveCountGreaterThanOrEqualTo(3);";
+        const string nfluentEquivalent = "Check.That(var).WhoseSize().IsGreaterOrEqualThan(3);";
+
+        string actual = CsFileContentReplacer.Replace(fluentAssertions);
+
+        Check.That(actual).IsEqualTo(nfluentEquivalent);
+    }  
+}
---
+++ UnitTests/CsFileContentReplacer/Collection/HaveCountGreaterThanTest.cs
@@ -0,0 +1,17 @@
+﻿using NFluent;
+
+namespace UnitTests.CsFileContentReplacer;
+
+public class HaveCountGreaterThanTest : CsTestContentReplacerTest
+{
+    [Fact]
+    public void Should_replace_HaveCountGreaterThan()
+    {
+        const string fluentAssertions = "var.Should().HaveCountGreaterThan(3);";
+        const string nfluentEquivalent = "Check.That(var).WhoseSize().IsStrictlyGreaterThan(3);";
+
+        string actual = CsFileContentReplacer.Replace(fluentAssertions);
+
+        Check.That(actual).IsEqualTo(nfluentEquivalent);
+    }  
+}
